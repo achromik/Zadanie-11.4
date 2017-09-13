dccb94624acc844a276cbb83518c594a5a8c9750
Aleksander Chromik <achromik@gmail.com>
2017-09-14 01:11:41 +0200

    Some more methods and properitiess to class Phone

---
+++ README.md
@@ -0,0 +1,4 @@
+# zadanie-11.4
+
+
+#### Zadanie 11.4 z kodilla.com OOP - first class
---
+++ js/scripts.js
@@ -0,0 +1,89 @@
+function Phone(brand, price, color ) {
+    this.brand = brand;
+    this.price = price;
+    this.color= (color!=undefined ? color : "No info");
+    this.VAT = 23;
+    accesories = [];
+	powerState = 0;
+	OS = undefined;
+}
+
+Phone.prototype = {
+	
+	printInfo : function () {
+		console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and price is ' + this.price + '.');
+		if(this.OS != undefined) {
+			console.log('OS is ' + this.OS);
+		}
+	},
+
+	setOS : function(OS) {
+		this.OS = OS;
+	},
+
+	getPrice : function() {
+		var self = this;
+		return self.price;
+	},
+
+	setVAT : function(VAT) {
+		this.VAT = VAT;
+	},
+
+	getVat : function() {
+		return this.VAT;
+	},
+
+	getTotalPrice : function () {
+		return this.price+this.price*this.VAT/100;
+	},
+
+	setAccesories : function(accesories) {
+		this.accesories = [];
+		for( var i =0; i<arguments.length; i++){
+			this.accesories.push(arguments[i]);
+		}
+	},
+
+	addAccesories : function(accesories) {
+		for( var i =0; i<arguments.length; i++){
+			this.accesories.push(arguments[i]);
+		}
+	},
+
+	powerToggle : function() {
+		this.powerState = (powerState) ? 0 : 1;
+	},
+
+	getPowerState : function() {
+		return (this.powerState) ? 'ON' : 'OFF';
+	},
+};
+
+
+var S5 = new Phone ('Samsung', 100, "Silver");
+var a5 = new Phone ('Apple 5s', "red");
+
+S5.printInfo();
+S5.setOS("android 4.4");
+S5.printInfo();
+S5.setAccesories('headphones', 'power adapter');
+console.log(S5.accesories);
+S5.addAccesories('etui', 'box');
+console.log(S5.accesories);
+console.log(S5);
+
+console.log('Phone ' + S5.brand + ' is ' + S5.getPowerState());
+S5.powerToggle();
+console.log('Phone ' + S5.brand + ' is ' + S5.getPowerState());
+console.log(S5);
+
+
+
+
+console.log('Price whit Tax: ' + S5.getTotalPrice());
+
+
+a5.printInfo();
+a5.setOS("OSX");
+a5.printInfo();
